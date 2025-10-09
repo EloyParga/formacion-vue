1ebd674c1d21122183c431656d41b65d4aabf107
Eloy Parga <sara.fernandez@vitesia.com>
2025-10-09 09:12:05 +0200

    Primer commit - formación Vue

---
+++ 03-directivas/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>Directivas Vue</h1>
+    <DirectivasSelectivas />
+  </div>
+</template>
+
+<script setup>
+  import DirectivasSelectivas from './components/DirectivasSelectivas.vue';
+</script>
+
+<style scoped>
+  h1 {
+    text-align: center;
+    color: #42b983;
+  }
+</style>
---
+++ 03-directivas/src/components/DirectivasSelectivas.vue
@@ -0,0 +1,22 @@
+/* Ejemplo de Renderizados Selectivos  ( v-if, v-else, v-else-if, v-show)*/
+// Las Directivas van a estar relacionados con el renderizado del Virtual DOM
+
+<template>
+    <div>
+        <h2>Renderizados Selectivos</h2>
+        
+        <p v-if="age >= 18">Eres mayor de edad</p>
+        <p v-else>Eres menor de edad</p>
+        <p v-show="age >= 18">Eres mayor de edad</p>
+        <p v-show="age < 18">Eres menor de edad</p>
+
+    </div>
+</template>
+
+<script setup>
+    let age = 10;
+</script>
+
+<style scope>
+
+</style>
