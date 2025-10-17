e523faf8be3d5a260ae2c83f7a426bb7a5302489
Eloy Parga <sara.fernandez@vitesia.com>
2025-10-17 14:31:17 +0200

    Update TEMA 10 con ruta dinamica y parametros

--- 10-enrutado/src/views/ArticuloView.vue
+++ 10-enrutado/src/views/ArticuloView.vue
@@ -3,5 +3,17 @@
         <h1>Artículo de Blog</h1>
         <p>Has seleccionado el artículo con ID: {{ $route.params.id }}</p>
         <p>Contenido del artículo...</p>
+        <p>ID del artículo usando ref: {{ articleId }}</p>
     </div>
 </template>
+
+<script setup>
+    import { onMounted , ref} from 'vue';
+    import { useRoute } from 'vue-router';
+    
+    let articleId = ref('');
+    onMounted(() =>{
+        const $route = useRoute();
+        articleId.value = $route.params.id;
+    })
+</script>
--- 10-enrutado/src/views/BlogView.vue
+++ 10-enrutado/src/views/BlogView.vue
@@ -4,9 +4,9 @@
         <p>Bienvenido a nuestro blog. Aquí encontrarás las últimas noticias y artículos.</p>
 
         <ul>
-            <li><RouterLink to="/blog/articulo-1">Artículo 1</RouterLink></li>
-            <li><RouterLink to="/blog/articulo-2">Artículo 2</RouterLink></li>
-            <li><RouterLink to="/blog/articulo-3">Artículo 3</RouterLink></li>
+            <li><RouterLink :to= "{ name: 'articulo', params: { id: 1 }}">Artículo 1 </RouterLink></li>
+            <li><RouterLink :to= "{ name: 'articulo', params: { id: 2 }}">Artículo 2</RouterLink></li>
+            <li><RouterLink :to= "{ name: 'articulo', params: { id: 3 }}">Artículo 3</RouterLink></li>
         </ul>
     </div>
 </template>
